b7b5dc76b4e10331b5953e00fb1f80062b9de2de
Misha-jordan <132101205+Misha-jordan@users.noreply.github.com>
2023-10-29 18:52:07 +0300

    Delete lab1 directory

--- lab1/Kalugin.Mihail/lab1.1.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-void taskOne();
-bool checkForBadSymbol(char testedChar);
-std::string sortString(std::string s);
--- lab1/Kalugin.Mihail/lab1.2.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void taskTwo();
-int fibNumber(int n);
-int hosoyaNumber(int* n, int* i);
